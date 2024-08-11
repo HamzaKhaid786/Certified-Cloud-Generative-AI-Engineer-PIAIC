e232e71ecfaedd7603df78116e2bdd9ad237f8f9
Hamza Khalid <competitivebountyhunter@gmail.com>
2024-08-11 02:07:05 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+# Calculating the sum of 'first_number' and 'second_number'
+sum = first_number + second_number
+print("Sum is: ", sum)
+
+# Calculating the difference between 'first_number' and 'second_number'
+sub = first_number - second_number
+print("Sub is: ", sub)
+
+# Calculating the division of 'first_number' by 'second_number'
+div = first_number / second_number
+print("Division is: ", div)
+# Calculating the multiplication of 'first_number' and 'second_number'
+mul = first_number * second_number
+print("Multilpication is: ", mul)
+# Calculating the exponentiation of 'first_number' to the power of 'second_number'
+pow = first_number ** second_number
+print("Exponentiation is: ", pow)
+
+# # Calculating the floor division of 'first_number' by 'second_number'
+flodiv = first_number // second_number
+print("Floor Division is: ", flodiv)
+
+# Calculating the Modulo    
+mod = first_number % second_number
+print("Modulo is: ", mod)
+
